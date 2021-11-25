c7948169699cf0f52b39e26203775c87a63d0316
PARVATI LAXMI DURGA SAI SOUMYA CHITTOORY <79699495+Soumya1219@users.noreply.github.com>
2021-11-26 00:41:01 +0530

    Add files via upload

---
+++ Searching/Frequency of Unique elements.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class sample{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter size of the array : ");
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		int i,j,c = 0,original;
+		System.out.println("Enter elements in array :");
+		for(i = 0;i < n;i++) {
+			arr[i] = sc.nextInt();
+		}
+		original = 0;
+		for(i = 0;i < n;i++){
+			for(j = 0;j < n;j++) {
+				if(arr[i] == arr[j] && i != j) {
+					break;
+					
+					
+				}		
+			}
+			if(j == n) {
+				c += 1;
+			}
+			
+			
+		}
+			System.out.println(c);
+	}
+		
+}
